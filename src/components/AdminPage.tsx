a2ff6164d5b9f84bfead278b3fe74ecc0a895780
Alter <jf.lozano.n@gmail.com>
2026-04-13 14:08:19 -0600

    feat: add new feature

--- src/components/AdminPage.tsx
+++ src/components/AdminPage.tsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from 'react';
+import type { Product } from '../types/types';
 import "../styles/global.css";
 import DisplayCard from "../components/ui/displayCard";
 import ProductForm from "./ProductForm";
+import { supabase } from '../lib/supabase';
 
 export default function AdminPage() {
+  const [productos, setProductos] = useState<Product[] | null>();
+
+  const onSave = () => {
+    fetch();
+  }
+
+  const fetch = async () => {
+    const { data, error } = await supabase
+      .from('products')
+      .select('*')
+    if (!error && data) {
+      setProductos(data);
+    }
+  }
+
+  useEffect(() => {
+    //fetch
+    fetch();
+  }, [])
+
   return <div className="bg-blue-mirage max-w-full h-screen flex flex-col items-start">
     <h1 className="text-3xl mx-auto my-2">Administrar</h1>
     <DisplayCard />
-    <ProductForm />
+    <ProductForm onSave={onSave} producto={{} as Product} />
   </div>
 }
